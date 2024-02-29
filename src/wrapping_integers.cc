cc96c0c807c74bf7ececf399d817298c473f8450
Msstt <1064263053@qq.com>
2024-02-29 20:27:58 +0800

    p2

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -4,16 +4,15 @@ using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  return Wrap32 { zero_point + n };
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  uint32_t dist = raw_value_ - zero_point.raw_value_ - checkpoint;
+  uint64_t result = checkpoint + dist;
+  if (dist > (1u << 31) && result >= (1ul << 32)) {
+    result -= (1ul << 32);
+  }
+  return result;
 }

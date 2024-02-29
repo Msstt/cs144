6d1a2a06d684f533b1073a57387634a9f3e19e94
Msstt <1064263053@qq.com>
2024-02-29 22:00:54 +0800

    p2

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -4,12 +4,35 @@ using namespace std;
 
 void TCPReceiver::receive( TCPSenderMessage message )
 {
-  // Your code here.
-  (void)message;
+  if (message.RST) {
+    reassembler_.set_error();
+  }
+  if (reassembler_.writer().has_error()) {
+    return;
+  }
+  if (message.SYN) {
+    started_ = true;
+    isn_ = message.seqno;
+  }
+  if (!started_) {
+    return;
+  }
+  auto index = message.seqno.unwrap(isn_, reassembler_.expect_index());
+  if (!message.SYN) {
+    index -= 1;
+  }
+  reassembler_.insert(index, message.payload, message.FIN);
 }
 
 TCPReceiverMessage TCPReceiver::send() const
 {
-  // Your code here.
-  return {};
+  TCPReceiverMessage result;
+  if (started_) {
+    result.ackno = Wrap32::wrap(reassembler_.expect_index(), isn_) + (reassembler_.writer().is_closed() ? 2 : 1);
+  } else {
+    result.ackno = nullopt;
+  }
+  result.window_size = min(static_cast<uint64_t>(UINT16_MAX), reassembler_.writer().available_capacity());
+  result.RST = reassembler_.writer().has_error();
+  return result;
 }

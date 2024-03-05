af1b37373756abbdb6b364fa4990b66a4b6397f7
Msstt <1064263053@qq.com>
2024-03-05 14:18:46 +0800

    p5

--- src/network_interface.hh
+++ src/network_interface.hh
@@ -1,10 +1,34 @@
 #pragma once
 
+#include <set>
 #include <queue>
+#include <map>
 
 #include "address.hh"
 #include "ethernet_frame.hh"
 #include "ipv4_datagram.hh"
+
+using ip32 = uint32_t;
+
+class NetworkInterfaceTimer {
+public:
+    NetworkInterfaceTimer(uint64_t expire_ms) : expire_ms_(expire_ms) {}
+
+    void update(ip32 ip_add, EthernetAddress eth_add);
+
+    void tick(size_t ms_since_last_tick);
+
+    bool exist(ip32 ip_add);
+
+    EthernetAddress get(ip32 ip_add);
+
+private:
+    uint64_t expire_ms_;
+    std::map<ip32, uint64_t> last_ {};
+    std::set<std::pair<uint64_t, ip32>> order_ {};
+    std::map<ip32, EthernetAddress> data_ {};
+    uint64_t now_time_ {};
+};
 
 // A "network interface" that connects IP (the internet layer, or network layer)
 // with Ethernet (the network access layer, or link layer).
@@ -30,55 +54,64 @@
 class NetworkInterface
 {
 public:
-  // An abstraction for the physical output port where the NetworkInterface sends Ethernet frames
-  class OutputPort
-  {
-  public:
-    virtual void transmit( const NetworkInterface& sender, const EthernetFrame& frame ) = 0;
-    virtual ~OutputPort() = default;
-  };
+    // An abstraction for the physical output port where the NetworkInterface sends Ethernet frames
+    class OutputPort
+    {
+    public:
+        virtual void transmit( const NetworkInterface& sender, const EthernetFrame& frame ) = 0;
+        virtual ~OutputPort() = default;
+    };
 
-  // Construct a network interface with given Ethernet (network-access-layer) and IP (internet-layer)
-  // addresses
-  NetworkInterface( std::string_view name,
-                    std::shared_ptr<OutputPort> port,
-                    const EthernetAddress& ethernet_address,
-                    const Address& ip_address );
+    // Construct a network interface with given Ethernet (network-access-layer) and IP (internet-layer)
+    // addresses
+    NetworkInterface( std::string_view name,
+                      std::shared_ptr<OutputPort> port,
+                      const EthernetAddress& ethernet_address,
+                      const Address& ip_address );
 
-  // Sends an Internet datagram, encapsulated in an Ethernet frame (if it knows the Ethernet destination
-  // address). Will need to use [ARP](\ref rfc::rfc826) to look up the Ethernet destination address for the next
-  // hop. Sending is accomplished by calling `transmit()` (a member variable) on the frame.
-  void send_datagram( const InternetDatagram& dgram, const Address& next_hop );
+    // Sends an Internet datagram, encapsulated in an Ethernet frame (if it knows the Ethernet destination
+    // address). Will need to use [ARP](\ref rfc::rfc826) to look up the Ethernet destination address for the next
+    // hop. Sending is accomplished by calling `transmit()` (a member variable) on the frame.
+    void send_datagram( const InternetDatagram& dgram, const Address& next_hop );
 
-  // Receives an Ethernet frame and responds appropriately.
-  // If type is IPv4, pushes the datagram to the datagrams_in queue.
-  // If type is ARP request, learn a mapping from the "sender" fields, and send an ARP reply.
-  // If type is ARP reply, learn a mapping from the "sender" fields.
-  void recv_frame( const EthernetFrame& frame );
+    // Receives an Ethernet frame and responds appropriately.
+    // If type is IPv4, pushes the datagram to the datagrams_in queue.
+    // If type is ARP request, learn a mapping from the "sender" fields, and send an ARP reply.
+    // If type is ARP reply, learn a mapping from the "sender" fields.
+    void recv_frame( const EthernetFrame& frame );
 
-  // Called periodically when time elapses
-  void tick( size_t ms_since_last_tick );
+    // Called periodically when time elapses
+    void tick( size_t ms_since_last_tick );
 
-  // Accessors
-  const std::string& name() const { return name_; }
-  const OutputPort& output() const { return *port_; }
-  OutputPort& output() { return *port_; }
-  std::queue<InternetDatagram>& datagrams_received() { return datagrams_received_; }
+    // Accessors
+    const std::string& name() const { return name_; }
+    const OutputPort& output() const { return *port_; }
+    OutputPort& output() { return *port_; }
+    std::queue<InternetDatagram>& datagrams_received() { return datagrams_received_; }
 
 private:
-  // Human-readable name of the interface
-  std::string name_;
+    // Human-readable name of the interface
+    std::string name_;
 
-  // The physical output port (+ a helper function `transmit` that uses it to send an Ethernet frame)
-  std::shared_ptr<OutputPort> port_;
-  void transmit( const EthernetFrame& frame ) const { port_->transmit( *this, frame ); }
+    // The physical output port (+ a helper function `transmit` that uses it to send an Ethernet frame)
+    std::shared_ptr<OutputPort> port_;
+    void transmit( const EthernetFrame& frame ) const { port_->transmit( *this, frame ); }
 
-  // Ethernet (known as hardware, network-access-layer, or link-layer) address of the interface
-  EthernetAddress ethernet_address_;
+    // Ethernet (known as hardware, network-access-layer, or link-layer) address of the interface
+    EthernetAddress ethernet_address_;
 
-  // IP (known as internet-layer or network-layer) address of the interface
-  Address ip_address_;
+    // IP (known as internet-layer or network-layer) address of the interface
+    Address ip_address_;
 
-  // Datagrams that have been received
-  std::queue<InternetDatagram> datagrams_received_ {};
+    // Datagrams that have been received
+    std::queue<InternetDatagram> datagrams_received_ {};
+
+    NetworkInterfaceTimer ARP_ {5000};
+    NetworkInterfaceTimer add_ {30000};
+
+    std::map<ip32, std::queue<InternetDatagram>> need_ {};
+
+    void transmit_IPv4( const InternetDatagram& dgram, EthernetAddress dst);
+
+    void transmit_ARP(ip32 dst, uint16_t opcode);
 };
--- src/router.cc
+++ src/router.cc
@@ -16,15 +16,46 @@ void Router::add_route( const uint32_t route_prefix,
                         const optional<Address> next_hop,
                         const size_t interface_num )
 {
-  cerr << "DEBUG: adding route " << Address::from_ipv4_numeric( route_prefix ).ip() << "/"
-       << static_cast<int>( prefix_length ) << " => " << ( next_hop.has_value() ? next_hop->ip() : "(direct)" )
-       << " on interface " << interface_num << "\n";
+//  cerr << "DEBUG: adding route " << Address::from_ipv4_numeric( route_prefix ).ip() << "/"
+//       << static_cast<int>( prefix_length ) << " => " << ( next_hop.has_value() ? next_hop->ip() : "(direct)" )
+//       << " on interface " << interface_num << "\n";
 
-  // Your code here.
+    rule_.push_back(Route{route_prefix, prefix_length, next_hop, interface_num});
+}
+
+size_t Router::find_index(uint64_t ip_add) {
+    int p = - 1;
+    for (size_t i = 0; i < rule_.size(); i++) {
+        int len = 32 - rule_[i].prefix_length_;
+        if (len == 32 || ((ip_add & rule_[i].route_prefix_) >> len) == ((ip_add | rule_[i].route_prefix_) >> len)) {
+            if (p == - 1 || rule_[i].prefix_length_ > rule_[p].prefix_length_) {
+                p = i;
+            }
+        }
+    }
+    return p;
 }
 
 // Go through all the interfaces, and route every incoming datagram to its proper outgoing interface.
 void Router::route()
 {
-  // Your code here.
+    for (size_t i = 0; i < _interfaces.size(); i++) {
+        auto &que = interface(i)->datagrams_received();
+        while (!que.empty()) {
+            auto &dgram = que.front();
+            if (dgram.header.ttl > 1) {
+                dgram.header.ttl -= 1;
+                dgram.header.compute_checksum();
+                auto index = find_index(dgram.header.dst);
+                std::cout << index << std::endl;
+                auto interface_num = rule_[index].interface_num_;
+                if (rule_[index].next_hop_.has_value()) {
+                    interface(interface_num)->send_datagram(dgram, *rule_[index].next_hop_);
+                } else {
+                    interface(interface_num)->send_datagram(dgram, Address::from_ipv4_numeric(dgram.header.dst));
+                }
+            }
+            que.pop();
+        }
+    }
 }
